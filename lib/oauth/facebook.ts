49c0fd76ae29732c2081a78d5c740f71fc097d69
jt01052552295 <jt01052552295@gmail.com>
2026-01-21 15:11:21 +0900

    facebook.ts 수정

--- lib/oauth/facebook.ts
+++ lib/oauth/facebook.ts
@@ -5,9 +5,9 @@ export const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID || '';
 export const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET || '';
 export const FACEBOOK_REDIRECT_URI = process.env.FACEBOOK_REDIRECT_URI || '';
 
-export const FACEBOOK_AUTH_URL = 'https://www.facebook.com/v22.0/dialog/oauth';
+export const FACEBOOK_AUTH_URL = 'https://www.facebook.com/v24.0/dialog/oauth';
 export const FACEBOOK_TOKEN_URL =
-  'https://graph.facebook.com/v22.0/oauth/access_token';
+  'https://graph.facebook.com/v24.0/oauth/access_token';
 export const FACEBOOK_PROFILE_URL = 'https://graph.facebook.com/me';
 export const FACEBOOK_REVOKE_URL = (userId: string) =>
   `https://graph.facebook.com/${userId}/permissions`;
@@ -51,7 +51,7 @@ export async function getFacebookToken(code: string) {
 
 // 사용자 정보 조회
 export async function getFacebookProfile(accessToken: string) {
-  const url = `${FACEBOOK_PROFILE_URL}?fields=id,name,email,picture&access_token=${accessToken}`;
+  const url = `${FACEBOOK_PROFILE_URL}?fields=id,name,email&access_token=${accessToken}`;
   const response = await fetch(url);
   return await response.json();
 }
@@ -111,11 +111,6 @@ export interface FacebookUser {
   id: string;
   name: string;
   email?: string;
-  picture?: {
-    data: {
-      url: string;
-    };
-  };
 }
 
 export interface FacebookTokenResponse {
